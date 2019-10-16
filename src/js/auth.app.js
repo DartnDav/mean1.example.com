6ac7f35d2a0b232e1e7b8b628e0352dd6e63a587
David-Halsey <dartndav@aol.com>
2019-10-15 21:25:12 -0500

    Add error/success handling to the login form

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -50,7 +50,12 @@ var authApp = (function () {
       xhr.send(JSON.stringify(object));
       xhr.onload = function () {
         let data = JSON.parse(xhr.response);
-        console.log(data);
+        //~line 52
+        if (data.success === true) {
+          window.location.href = '/';
+        } else {
+          document.getElementById('formMsg').style.display = 'block';
+        }
       }
     });
   }
